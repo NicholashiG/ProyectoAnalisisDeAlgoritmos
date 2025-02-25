4a980d304ceb056c4258280d0cb53e66e318ad01
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2025-02-25 17:30:34 -0500

    Cambios menores

--- src/main/java/co/edu/uniquindio/analisisalgoritmos/proyecto/Main.java
+++ src/main/java/co/edu/uniquindio/analisisalgoritmos/proyecto/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package co.edu.uniquindio.analisisalgoritmos.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        System.out.println("Hello World!");
+
+        System.out.println("Hello world!");
+
     }
 }
